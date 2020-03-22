77e50a0a1fee9b1337123b0a457246aedea79deb
szb15137 <siqi_zhang_2017@163.com>
2020-03-22 21:47:52 +0800

    Add if clause example.

--- ex.py
+++ ex.py
@@ -2,7 +2,7 @@
 
 from operator import floordiv, mod
 
-def divide_exact(n, d):
+def divide_exact(n, d = 10):
     """ Return the quotient and remainder of dividing N by D.
 
     >>> q, r  = divide_exact(2013, 10)
@@ -12,3 +12,12 @@ def divide_exact(n, d):
     3
     """
     return floordiv(n, d), mod(n, d)
+
+def absolute_value(x):
+    """Return the absolute value of x."""
+    if x < 0:
+        return -x
+    elif x == 0:
+        return 0
+    else:
+        return x
